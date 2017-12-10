2410854fc2e0861c891a271dbfe8d3e28f46938f
Daniel <danielrferriss@gmail.com>
2017-12-09 18:46:43 -0600

    Basic Web Design

---
+++ navbar.js
@@ -0,0 +1,5 @@
+document.getElementById("navMenu").innerHTML = 
+'<ul>' +
+  '<li class="default"><a href="index.html">Game</a></li>' +
+  '<li class="default"><a href="settings.html">Settings</a></li>' +
+'</ul>';
