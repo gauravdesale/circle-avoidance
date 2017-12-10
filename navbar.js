fea22af2b8b42663409c88f5973675916c8b647a
Daniel <danielrferriss@gmail.com>
2017-12-09 21:19:47 -0600

    navigation bar

--- navbar.js
+++ navbar.js
@@ -2,4 +2,5 @@ document.getElementById("navMenu").innerHTML =
 '<ul>' +
   '<li class="default"><a href="index.html">Game</a></li>' +
   '<li class="default"><a href="settings.html">Settings</a></li>' +
+  '<li class="default"><a href="howtoplay.html">How to Play</a></li>' +
 '</ul>';
